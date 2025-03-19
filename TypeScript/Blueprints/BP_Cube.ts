8c29ccf715de1f84a92f0c23f5e200abf65d0869
陈华 <2394439184@qq.com>
2025-03-20 01:08:19 +0800

    继承关系以及会出现的BUG完全解决

--- TypeScript/Blueprints/BP_Cube.ts
+++ TypeScript/Blueprints/BP_Cube.ts
@@ -1,33 +1,28 @@
 import * as UE from 'ue';
 import {blueprint} from 'puerts';
 
-import {KismetSystemLibrary} from 'ue';
-
-// 加载蓝图类
-const uclass = UE.Class.Load("/Game/Blueprints/BP_Cube.BP_Cube_C");
-
-// 转换为js类
-const jsClass = blueprint.tojs<typeof UE.Game.Blueprints.BP_Cube.BP_Cube_C>(uclass);
+// 加载子类蓝图（转换成js类）
+const Uclass = UE.Class.Load("/Game/Blueprints/BP_Cube.BP_Cube_C");
+const JsClass = blueprint.tojs<typeof UE.Game.Blueprints.BP_Cube.BP_Cube_C>(Uclass);
 
 // 创建一个继承ts类的接口（用来类型提示）
 interface TS_Cube extends UE.Game.Blueprints.BP_Cube.BP_Cube_C {
 }
 
-// 创建一个继承ts的本体类   export 导出
-export class ts_Cube extends jsClass implements TS_Cube {
-
+// 创建一个继承ts的本体类   export 导出  extends 扩展（可以理解成继承哪一个类）  implements   实现类型提示
+export class BP_Cube extends JsClass implements TS_Cube {
 
     StartRotation: boolean;
 
     // 开始函数
-    override ReceiveBeginPlay(): void {
+    ReceiveBeginPlay(): void {
         console.log("这是来自于ts_Cube ReceiveBeginPlay 的消息");
 
         this.OnOverlap();
     }
 
     // Tick函数
-    override ReceiveTick(DeltaSeconds: number): void {
+    ReceiveTick(DeltaSeconds: number): void {
         if (this.StartRotation) {
             this.K2_AddActorWorldRotation(new UE.Rotator(0, 30 * DeltaSeconds, 0), false, undefined, false);
         }
@@ -36,7 +31,7 @@ export class ts_Cube extends jsClass implements TS_Cube {
 
     // 点击函数
     public override Click(): void {
-        KismetSystemLibrary.PrintString(this.GetWorld(), "你点你妈呢", true, true, new UE.LinearColor(1, 0, 0, 1), 2.0);
+        UE.KismetSystemLibrary.PrintString(this.GetWorld(), "你点你妈呢", true, true, new UE.LinearColor(1, 0, 0, 1), 2.0);
         this.StartRotation = !this.StartRotation;
     }
 
@@ -45,7 +40,7 @@ export class ts_Cube extends jsClass implements TS_Cube {
         this.Cube.OnComponentBeginOverlap.Add((overlappedComp, otherActor, otherComp, otherBodyIndex, fromSweep, sweepResult) => {
 
             let Name = otherActor.GetName();
-            KismetSystemLibrary.PrintString(
+            UE.KismetSystemLibrary.PrintString(
                 this.GetWorld(),
                 `我是与:${Name}重叠了！！！ `, // 类似于C++的  "%s",*otherActor.GetName()
                 true,
@@ -58,4 +53,4 @@ export class ts_Cube extends jsClass implements TS_Cube {
 }
 
 // 让编辑器识别！！！非常重要！！！
-blueprint.mixin(jsClass, ts_Cube);
+blueprint.mixin(JsClass, BP_Cube);
--- TypeScript/Blueprints/BP_Cube_Child.ts
+++ TypeScript/Blueprints/BP_Cube_Child.ts
@@ -1,39 +1,37 @@
-﻿/*
-// BP_Cube_Child.ts
+﻿// BP_Cube_Child.ts
 import * as UE from 'ue';
-import { blueprint } from 'puerts';
-import { ts_Cube } from './BP_Cube';
+import {blueprint} from 'puerts';
+import {BP_Cube} from './BP_Cube'; // 先导入父类(有继承关系)
 
-// 加载子类蓝图（验证路径大小写）
-const uclassChild = UE.Class.Load("/Game/Blueprints/BP_Cube_Child.BP_Cube_Child_C");
-const jsClassChild = blueprint.tojs<typeof UE.Game.Blueprints.BP_Cube_Child.BP_Cube_Child_C>(uclassChild);
+// 加载子类蓝图（转换成js类）
+const Uclass = UE.Class.Load("/Game/Blueprints/BP_Cube_Child.BP_Cube_Child_C");
+const JsClass = blueprint.tojs<typeof UE.Game.Blueprints.BP_Cube_Child.BP_Cube_Child_C>(Uclass);
 
-// 桥接类继承UE蓝图JS类
-class ChildBridge extends jsClassChild {}
+// 创建一个继承ts类的接口（用来类型提示）
+interface TS_CubeChild extends UE.Game.Blueprints.BP_Cube_Child.BP_Cube_Child_C {
+}
 
-export class ts_CubeChild extends ts_Cube {
-    // 必须保留父类属性（根据ue.d.ts自动生成）
-    StartRotation!: boolean; // 从父类继承的属性
+// 直接继承JS基类    export 导出  extends 扩展（可以理解成继承哪一个类）  implements  实现类型提示
+// @ts-ignore
+export class BP_CubeChild extends BP_Cube implements TS_CubeChild {
 
-    override ReceiveBeginPlay(): void {
-        super.ReceiveBeginPlay(); // 关键！调用父类蓝图逻辑
-        console.log("子类BeginPlay触发");
-        
+    ReceiveBeginPlay(): void {
+        // super.ReceiveBeginPlay(); // 调用蓝图父类方法
+        console.log("我是子类");
+        this.OnOverlap();
     }
 
-    override Click(): void {
-        super.Click(); // 保留父类点击旋转逻辑
-        UE.KismetSystemLibrary.PrintString(
-            this.GetWorld(),
-            "子类点击特效",
-            true,
-            true,
-            new UE.LinearColor(0, 1, 1, 1),
-            2
-        );
-        
+    Click(): void {
+        super.Click();
+        console.log("子类点击");
+    }
+
+    // 子类拥有的函数
+    Click2(): void {
+        UE.KismetSystemLibrary.PrintString(this.GetWorld(), "我就点我就点", true, true, new UE.LinearColor(0, 1, 0, 1), 2.0);
+        this.StartRotation = !this.StartRotation;
     }
 }
 
-// 关键混入操作（必须在文件末尾）
-blueprint.mixin(jsClassChild, jsClassChild);*/
+// @ts-ignore   不加@ts-ignore会有报错提示，仅仅只是提示，没有任何错误信息（仅限继承时候的mixin）
+blueprint.mixin(JsClass, BP_CubeChild);
